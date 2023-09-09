0d7cb2fcab7fb93fb5d782907ffbe6e25bbe03c4
Abigail <abigail@dogbuilt.net>
2023-09-09 14:18:13 -0400

    Add zh_CN to scripts/update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,7 +3,7 @@
 
 set -ex
 
-LANG_TO_PULL=${1:-'ro_RO,pt,ja_JP,fr_CA'}
+LANG_TO_PULL=${1:-'ro_RO,pt,ja_JP,fr_CA,zh_CN'}
 LANG_MAP='ro_RO: ro, ja_JP: ja, fr_CA: fr'
 MAINPROJECT=gm0
 ORGANIZATION=gamemanual0
